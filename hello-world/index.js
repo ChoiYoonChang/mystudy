35add40cba096b7a8a196495c48198deadd41e5b
y.c.choi <yc2839@naver.com>
2019-08-23 23:47:05 +0900

    m

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
+
+app.use(express.static(__dirname + '/public')); // 1
 
 app.listen(3000, function(){
   console.log('Server On!');
