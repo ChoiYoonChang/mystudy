b025590d1f91a3aaaa7e51a4ac86d7d4690dba4f
y.c.choi <yc2839@naver.com>
2019-08-24 00:06:54 +0900

    m

--- hello-world/index.js
+++ hello-world/index.js
@@ -5,7 +5,16 @@ var app = express();
 //   res.send('Hello World!');
 // });
 
-app.use(express.static(__dirname + '/public')); // 1
+app.set("view engine",  "ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.get("/hello", function(req,res){
+  res.render("hello", {name:req.query.nameQuery});
+});
+
+app.get("/hello/:nameParam", function(req,res){
+  res.render("hello", {name:req.params.nameParam});
+});
 
 app.listen(3000, function(){
   console.log('Server On!');
